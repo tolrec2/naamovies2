18ac212d4cf59f8f4f2d38f11853c767dc7ebb3f
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-11-13 10:36:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/yogi444/MartinBot.git /MartinBot
+  git clone https://github.com/Mrcnu/MartinBot.git /MartinBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-search-tamil-bot
