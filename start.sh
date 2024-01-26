83bb5f9c1cad1e515ef4eb785d0b17f22830bf02
tolrec2 <156929168+tolrec2@users.noreply.github.com>
2024-01-26 17:28:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mrcnu/naamovies2.git /MartinBot
+  git clone https://git.heroku.com/naamoviez.git /MartinBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MartinBot
